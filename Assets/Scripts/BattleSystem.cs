4b0e13dbdeaea54ae3ae657dcffd3b8f0b1edad0
MattTiattoSAE <1023045@student.sae.edu.au>
2021-07-20 18:33:36 +1000

    Fixed most bugs :)

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Functions to complete:
+/// - Do Round
+/// - Fight Over
+/// </summary>
+public class BattleSystem : MonoBehaviour
+{
+    public DanceTeam teamA,teamB; //References to TeamA and TeamB
+    public FightManager fightManager; // References to our FightManager.
+
+    public float battlePrepTime = 2;  // the amount of time we need to wait before a battle starts
+    public float fightCompletedWaitTime = 2; // the amount of time we need to wait till a fight/round is completed.
+    
+    /// <summary>
+    /// This occurs every round or every X number of seconds, is the core battle logic/game loop.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator DoRound()
+    {     
+        // waits for a float number of seconds....
+        yield return new WaitForSeconds(battlePrepTime);
+
+        //checking for no dancers on either team
+        if(teamA.allDancers.Count == 0 && teamB.allDancers.Count == 0)
+        {
+            Debug.LogWarning("DoRound called, no dancers active. DanceTeamInit needs to be completed");
+            // This will be called if there are 0 dancers on both teams.
+
+        }
+        else if (teamA.activeDancers.Count > 0 && teamB.activeDancers.Count > 0)
+        {
+            Debug.LogWarning("DoRound called, dancers active");
+            //You need to select two random or engineered random characters to fight...so one from team a and one from team b....
+            Character dancerA = teamA.activeDancers[Random.Range(0, teamA.activeDancers.Count)];
+            Character dancerB = teamB.activeDancers[Random.Range(0, teamB.activeDancers.Count)];
+            //We then need to pass in the two selected dancers into fightManager.Fight(CharacterA,CharacterB);
+            
+            fightManager.Fight(dancerA, dancerB);
+            // we could also get fancy here by using the simulate battle first if we wanted to.
+        }
+        else
+        {
+            // IF we get to here...then we have a team has won...winner winner chicken dinner.
+            DanceTeam winner = null; 
+
+            // Checks both teams activeDancer count to see whether one team has eliminated the other
+            if (teamB.activeDancers.Count <= 0)
+            {
+                winner = teamA;
+            }
+            if (teamA.activeDancers.Count <= 0)
+            {
+                winner = teamB;
+            }
+            Debug.Log(teamA.danceTeamName + " wins!");
+
+            //Enables the win effects, and logs it out to the console.
+            winner.EnableWinEffects();
+            BattleLog.Log(winner.danceTeamName.ToString() + " wins!", winner.teamColor);
+
+            Debug.Log("Game Over");
+          
+        }
+        yield return null;
+    }
+
+    // This is where we can handle what happens when we win or lose.
+    public void FightOver(Character winner, Character defeated, float outcome)
+    {
+        Debug.LogWarning("FightOver called");
+        // assign damage...or if you want to restore health if they want that's up to you....might involve the character script.
+        if (outcome != 0f)
+        {
+            winner.DealDamage(0.25f - (outcome % 0.50f));
+            float damage = 0.25f - (outcome % 0.50f);
+            defeated.DealDamage(0.25f);
+            BattleLog.Log("Fight Over! \n" +
+                      "Winner: " + winner.charName.GetFullCharacterName() + " takes " + damage + " damage! \n" +
+                      "Loser: " + defeated.charName.GetFullCharacterName() + " takes 25 damage!", winner.myTeam.teamColor);
+        }
+        else
+        {
+            winner.DealDamage(0.10f);
+            defeated.DealDamage(0.10f);
+                        BattleLog.Log("Fight Over! \n No Winners! Both of y'all are taking 10 damage!", Color.grey);
+        }
+
+        //calling the coroutine so we can put waits in for anims to play
+        StartCoroutine(HandleFightOver());
+    }
+
+    /// <summary>
+    /// Used to Request A round.
+    /// </summary>
+    public void RequestRound()
+    {
+        //calling the coroutine so we can put waits in for anims to play
+        StartCoroutine(DoRound());
+    }
+
+    /// <summary>
+    /// Handles the end of a fight and waits to start the next round.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator HandleFightOver()
+    {
+        yield return new WaitForSeconds(fightCompletedWaitTime);
+        teamA.DisableWinEffects();
+        teamB.DisableWinEffects();
+        Debug.LogWarning("HandleFightOver called, may need to prepare or clean dancers or teams and checks before doing GameEvents.RequestFighters()");
+        RequestRound();
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,260 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Functions to complete:
+/// - Deal Damage
+/// </summary>
+public class Character : MonoBehaviour
+{
+    public CharacterName charName; // This is a reference to an instance of the characters name script.
+
+    [Range(0.0f,1.0f)]
+    public float mojoRemaining = 1; // This is the characters hp this is a float 0-100 but is normalised to 0.0 - 1.0;
+
+    [Header("Stats")]
+    /// <summary>
+    /// Our current level.
+    /// </summary>
+    public int level;
+
+    /// <summary>
+    /// The current amount of xp we have accumulated.
+    /// </summary>
+    public int currentXp;
+
+    /// <summary>
+    /// The amount of xp required to level up.
+    /// </summary>
+    public int xpThreshold = 10;
+
+    /// <summary>
+    /// The amount of points a character has to determine their initial stats
+    /// </summary>
+    public int pool = 20;
+
+    /// <summary>
+    /// Our variables used to determine our fighting power.
+    /// </summary>
+    public int style;
+    public int luck;
+    public int rhythm;
+
+    /// <summary>
+    /// Our physical stats that determine our dancing stats.
+    /// </summary>
+    public int agility;
+    public int intelligence;
+    public int strength;
+
+    /// <summary>
+    /// Used to determine the conversion of 1 physical stat, to 1 dancing stat.
+    /// </summary>
+    public float agilityMultiplier = 0.5f;
+    public float strengthMultiplier = 1f;
+    public float inteligenceMultiplier = 2f;
+
+    /// <summary>
+    /// A float used to display what the chance of winning the current fight is.
+    /// </summary>
+    public float percentageChanceToWin;
+
+
+    [Header("Related objects")]
+    public DanceTeam myTeam; // This holds a reference to the characters current dance team instance they are assigned to.
+
+    public bool isSelected; // This is used for determining if this character is selected in a battle.
+
+    [SerializeField]
+    protected TMPro.TextMeshPro nickText; // This is just a piece of text in Unity, to display the characters name.
+ 
+    public AnimationController animController; // A reference to the animationController, is used to switch dancing states.
+
+    // This is called once, this then calls Initial Stats function
+    void Awake()
+    {
+        animController = GetComponent<AnimationController>();
+        GeneratePhysicalStats(pool); // we want to generate some physical stats.
+        CalculateDancingStats();// using those physical stats we want to generate some dancing stats.
+    }
+
+    /// <summary>
+    /// Generates default stats for a character, using a stat pool
+    /// </summary>
+    /// <param name="pool"></param>
+    public void GeneratePhysicalStats(int pool)
+    {
+        Debug.LogWarning(this.gameObject.name + " | GeneratePhysicalStats called | " + pool);
+
+        while (pool > 0)
+        {
+            int statAssign = Random.Range(1, 4);
+            {
+                if (statAssign == 1)
+                {
+                    agility += 1;
+                }
+                if (statAssign == 2)
+                {
+                    strength += 1;
+                }
+                if (statAssign == 3)
+                {
+                    intelligence += 1;
+                }
+                pool -= 1;
+            }
+        }
+        Debug.Log(this.gameObject.name + " | GeneratePhysicalStats | Agility: " + agility + " | Strength: " + strength + " | Intelligence: " + intelligence);
+    }
+
+    /// <summary>
+    /// Generates a character's dancing stats based off their physical stats and the corresponding modifier
+    /// </summary>
+    public void CalculateDancingStats()
+    {
+        Debug.LogWarning(this.gameObject.name + " | CalculateDancingStats called");
+
+        style = (int)((float)agility * agilityMultiplier);
+
+        rhythm = (int)((float)strength * strengthMultiplier);
+
+        luck = (int)((float)intelligence * inteligenceMultiplier);
+
+        Debug.Log(this.gameObject.name + " | CalculateDancingStats | Style: " + style + " | Rhythm " + rhythm + " | Luck: " + luck);
+    }
+
+
+    /// <summary>
+    /// Generates a percentage value for a character's chance to win
+    /// </summary>
+    /// <param name="normalisedValue"></param>
+    public void SetPercentageValue(float normalisedValue)
+    {
+        Debug.LogWarning(this.gameObject.name + " | SetPercentageValue called | " + normalisedValue);
+
+        percentageChanceToWin = normalisedValue * 100;
+
+        Debug.Log(this.gameObject.name + " | SetPercentageValue | Normalised Value " + normalisedValue + " | Percentage " + percentageChanceToWin);
+    }
+
+    /// <summary>
+    /// Removes mojo from mojoRemaining equal to amount
+    /// Checks to
+    /// </summary>
+    public void DealDamage(float amount)
+    {
+        // we probably want to do a check in here to see if the character is dead or not...
+        // if they are we probably want to remove them from their team's active dancer list...sure wish there was a function in their dance team  script we could use for this.
+        mojoRemaining -= amount;
+        if (mojoRemaining <= 0)
+        {
+            myTeam.RemoveDancerFromActive(this);
+
+            Debug.LogWarning(this.gameObject + " has died!");
+        }
+    }
+
+/// <summary>
+/// Generates a power level for a character based on their power level (sum of dancing stats)
+/// </summary>
+/// <returns></returns>
+    public int ReturnDancePowerLevel()
+    {
+        Debug.LogWarning(this.gameObject.name + " | ReturnDancePowerLevel called");
+
+        float score = style + rhythm + luck;
+
+        Debug.Log(this.gameObject.name + " | ReturnDancePowerLevel | Power Level: " + score);
+
+        return (int)score;
+    }
+
+    /// <summary>
+    /// Adds xp to a character equal to xpGained
+    /// </summary>
+    /// <param name="xpGained"></param>
+    public void AddXP(int xpGained)
+    {
+        Debug.LogWarning(this.gameObject.name +  " | AddXP called | " + xpGained);
+        
+        currentXp += xpGained;
+
+        if (currentXp >= xpThreshold)
+        {
+            LevelUp();
+        }
+    }
+
+    /// <summary>
+    /// A function used to handle actions associated with levelling up.
+    /// </summary>
+    private void LevelUp()
+    {
+        Debug.LogWarning(this.gameObject.name + " | LevelUp called");
+
+        level += 1;
+        
+        currentXp -= xpThreshold;
+        
+        xpThreshold += xpThreshold + ((int)(level * 1.5));
+        
+        DistributePhysicalStatsOnLevelUp((int)((float)level * 2));
+
+        Debug.Log(this.gameObject.name + " | LevelUp | Level: " + level + " | CurrentXP: " + currentXp + " | XPThreshold: " + xpThreshold);
+
+        BattleLog.Log(charName.GetFullCharacterName() + " levelled up to level " + this.level, this.myTeam.teamColor);
+        if (currentXp >= xpThreshold)
+        {
+            Debug.LogWarning(this.gameObject.name + " | LevelUp | CurrentXP exceeds XPThreshold! | CurrentXP: " + currentXp + " | XPThreshold: " + xpThreshold);
+            LevelUp();
+        }
+    }
+
+    /// <summary>
+    /// Generates new physical stats for a character based on PointsPool
+    /// </summary>
+    /// <param name="PointsPool"></param>
+    public void DistributePhysicalStatsOnLevelUp(int PointsPool)
+    {
+        Debug.LogWarning(this.gameObject.name + " | DistributePhysicalStatsOnLevelUp has been called | " + pool);
+        while (PointsPool > 0)
+        {
+            int statAssign = Random.Range(1, 4);
+            {
+                if (statAssign == 1)
+                {
+                    agility += 1;
+                }
+                if (statAssign == 2)
+                {
+                    strength += 1;
+                }
+                if (statAssign == 2)
+                {
+                    intelligence += 1;
+                }
+            }
+            PointsPool -= 1;
+        }
+        Debug.Log(this.gameObject.name + " | DistributePhysicalStatsOnLevelUp | Agility: " + agility + " | Strength: " + strength + " | Intelligence: " + intelligence);
+        CalculateDancingStats();
+    }
+
+    /// <summary>
+    /// Is called inside of our DanceTeam.cs is used to set the characters name!
+    /// </summary>
+    /// <param name="characterName"></param>
+    public void AssignName(CharacterName characterName)
+    {
+        charName = characterName;
+        if(nickText != null)
+        {
+            nickText.text = charName.nickname;
+            nickText.transform.LookAt(Camera.main.transform.position);
+            //text faces the wrong way so
+            nickText.transform.Rotate(0, 180, 0);
+        }
+    }
+}
